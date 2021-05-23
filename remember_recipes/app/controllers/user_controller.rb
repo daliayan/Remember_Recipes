dceafc42926ba2bc52a94b07116e3c847558acde
daliayan <daliayan5@gmail.com>
2021-05-23 10:46:52 -0400

    Added to view files + added to controller files

--- remember_recipes/app/controllers/user_controller.rb
+++ remember_recipes/app/controllers/user_controller.rb
@@ -1,2 +1,22 @@
 class UserController < ApplicationController
+
+    get '/signup' do
+        erb :'users/new'
+    end
+
+    post '/signup' do
+        user = User.new(params)
+        if User.find_by_username(params[:username])
+            redirect '/taken_username'
+        elsif 
+            params[:username] == "" || params[:password] == ""
+            redirect '/error'
+        else
+            user.save
+            session[:user_id] = user.id
+            redirect '/recipes'
+        end
+    end
+
+
 end
